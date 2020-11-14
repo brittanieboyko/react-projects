a342105c0f1ca2471dd942d868fc14dcc40f9a3d
brittanieb <brittanie.boyko@gmail.com>
2020-11-13 20:36:26 -0500

    fixed formatting

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
   const [people, setPeople] = useState(data);
 
@@ -8,10 +8,8 @@ function App() {
     <main>
       <section className="container">
         <h3>{people.length} birthdays today</h3>
-        <List people={people}/>
-        <button onClick={()=> setPeople([])}>
-          clear all
-        </button>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
       </section>
     </main>
   );
