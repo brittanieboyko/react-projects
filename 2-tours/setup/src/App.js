5f0abc8cd515a25e0e7369a31f9f9f3a8dad8cf6
brittanieb <brittanie.boyko@gmail.com>
2020-11-18 18:28:35 -0500

    set up async function call to api

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -1,20 +1,31 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+const url = "https://course-api.com/react-tours-project";
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
-   if (loading) {
-     return ( <main>
-       <Loading />
-     </main> );
-   }
-   return <main>
-     <Tours />
-   </main>
+
+  const fetchTours = async () => {
+    setLoading(true);
+    const response = await fetch(url);
+    const tours = await response.json();
+    console.log(tours);
+  };
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
-export default App
+export default App;
